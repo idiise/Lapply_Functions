874575e23d113f2489190cce996230012329b342
Idrissa dabo <ididabo@gmail.com>
2020-12-25 02:01:32 +0000

    Fonction pour la catégorisation

--- Lapply sapply function.R
+++ Lapply sapply function.R
@@ -1,3 +1,15 @@
 data <- mtcars
 
 # Example of lapply 
+# Function to apply
+mpg_category <- function(mpg){
+  if (mpg > 30) {
+return("high")
+  } else if (mpg > 20) {
+    return("Medium")
+  }
+  return("Low")
+}
+
+# Apply to each element
+lapply(X = data$mpg, FUN = mpg_category)
